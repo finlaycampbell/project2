de73ae171fee1db945dd1ff7d5298a08290e32e1
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-04-14 17:17:56 +0100

    first commit

---
+++ R/functions.R
@@ -0,0 +1,10 @@
+#DEFINING FUNCTIONS
+
+say_hello <- function(your_name){
+  return(paste("Hello ",your_name,", how are you doing?",sep=""))
+}
+
+gamma_plot <- function(gamma_a,gamma_b){
+  temp_seq = seq(0,5,0.01)
+  return(qplot(temp_seq,dgamma(temp_seq,gamma_a,gamma_b),xlab="Value",ylab="Probability density"))
+}
---
+++ analysis.R
@@ -0,0 +1,6 @@
+source("R/functions.R")
+
+library(ggplot2)
+
+say_hello("Finlay")
+gamma_plot(5,5)
