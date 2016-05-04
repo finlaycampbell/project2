2c62f55a42da20a6d4040ad7cf915c4e57d7a488
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-05-04 11:59:56 +0100

    adding roxygen documentation to plot.simOutbreak

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,11 @@
-#DEFINING FUNCTIONS
+#' project2 - Functions
+#' 
+#' @author F. Campbell
+#' 
+#' @param R0 the effective reproduction number
+#' @param gamma_a the shape value of the gamma function used for w.dens
+#' @param gamma_b the rape values of the gamma function used for w.dens
 
-##Visualising simOutbreak
 plot.simOutbreak <- function(R0=2,gamma_a=4,gamma_b=1){
   temp.sim = simOutbreak(R0,dgamma(1:10,4,1))
   if(length(temp.sim$id)==1) return("No outbreak")
@@ -8,16 +13,21 @@ plot.simOutbreak <- function(R0=2,gamma_a=4,gamma_b=1){
   return(plot(temp.net))
 }
 
-##Returns the genetic pseudo-likelihood using the number of genetic differences, length of comparable genome,
-##number of generations and mutation rate
-mu.func = function(d,l,k,u){
+#Returns the genetic pseudo-likelihood using the number of genetic differences, length of comparable genome,
+#number of generations and mutation rate
+mu.func <- function(d,l,k,u){
   return(u^(d)*(1-u)^(k*l - d))
 }
 
-##Plots the likelihood given fixed d, l and k across a range of values for u, returning most likely u value
-plot.mu.func = function(d,l,k){
+#Plots the likelihood given fixed d, l and k across a range of values for u, returning most likely u value
+plot.mu.func <- function(d,l,k){
 temp.seq = seq(0,1,0.01)
-temp.output = mu.func(d,l,k,temp_seq)
-plot(temp.seq,temp/output,type='l')
+temp.output = mu.func(d,l,k,temp.seq)
+plot(temp.seq,temp.output,type='l',xlab="Mutation rate",ylab="Likelihood")
 return(temp.seq[which.max(temp.output)])
+}
+
+#Returns the probability of infection from simOutbreak given R0 and n (summing over w(t-ti))
+p.inf <- function(R0,n){
+  return(1 - exp(1)^(-R0/n))
 }
--- analysis.R
+++ analysis.R
@@ -13,5 +13,5 @@ test.dna = matrix(c("C","C","C","C","C","C","C","C","C",
                     "C","G","C","T","C","A","T","C","C"),nrow=5)
 
 test.date = c(0,4,7,10,14)
-test.w.dens = dgamma(1:10,2,0.5)
+test.w.dens = dgamma(1:11,2,0.5)
 test.output = outbreaker(test.dna,test.date,w.dens=test.w.dens)
