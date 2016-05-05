c20b7fd14749c729652c7c8a8790cdcb24b146e4
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-05-05 17:14:44 +0100

    added omega.3 (first try)

--- R/functions.R
+++ R/functions.R
@@ -4,12 +4,12 @@
 #' 
 #' @param R0 the effective reproduction number
 #' @param gamma_a the shape value of the gamma function used for w.dens
-#' @param gamma_b the rape values of the gamma function used for w.dens
+#' @param gamma_b the rate values of the gamma function used for w.dens
 
 plot.simOutbreak <- function(R0=2,gamma_a=4,gamma_b=1){
-  temp.sim = simOutbreak(R0,dgamma(1:10,4,1))
+  temp.sim <- simOutbreak(R0,dgamma(1:10,4,1))
   if(length(temp.sim$id)==1) return("No outbreak")
-  temp.net = graph.data.frame(cbind(temp.sim$ances[-1],temp.sim$id[-1]),directed=T)
+  temp.net <- graph.data.frame(cbind(temp.sim$ances[-1],temp.sim$id[-1]),directed=T)
   return(plot(temp.net))
 }
 
@@ -22,7 +22,7 @@ mu.func <- function(d,l,k,u){
 #Plots the likelihood given fixed d, l and k across a range of values for u, returning most likely u value
 plot.mu.func <- function(d,l,k){
 temp.seq = seq(0,1,0.01)
-temp.output = mu.func(d,l,k,temp.seq)
+temp.output <- mu.func(d,l,k,temp.seq)
 plot(temp.seq,temp.output,type='l',xlab="Mutation rate",ylab="Likelihood")
 return(temp.seq[which.max(temp.output)])
 }
@@ -30,4 +30,8 @@ return(temp.seq[which.max(temp.output)])
 #Returns the probability of infection from simOutbreak given R0 and n (summing over w(t-ti))
 p.inf <- function(R0,n){
   return(1 - exp(1)^(-R0/n))
+}
+
+omega.3 <- function(cij,eps){
+  return(eps^cij + eps*(cij-1))
 }
